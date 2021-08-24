d31153f3a6b1b149619d917f9bc7dcab6fa0dcc7
Stephen <sjohnston.ie@gmail.com>
2021-08-24 23:53:53 +0000

    feat: Update layout of grants.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,30 +1,29 @@
 $(document).ready(function () {
-  $('.sidenav').sidenav();
-  $('select').formSelect();
-  $('.collapsible').collapsible();
-  $('.tabs').tabs();
-
+  
   $('#tab1').show();
   $('#tab2').hide();
   $('#tab3').hide();
 
-  $('#tab1').click(function () {
+  $('tab1').click(function () {
     $('#tab1').show();
-    $('tab2').hide();
-    $('tab3').hide();
+    $('#tab2').show();
+    $('#tab3').show();
   });
 
-  $('#tab2').click(function () {
+  $('tab2').click(function () {
     $('#tab1').hide();
-    $('tab2').show();
-    $('tab3').hide();
+    $('#tab2').show();
+    $('#tab3').hide();
   });
 
-  $('#tab3').click(function () {
+  $('tab3').click(function () {
     $('#tab1').hide();
-    $('tab2').hide();
-    $('tab3').show();
+    $('#tab2').hide();
+    $('#tab3').show();
   });
 
-
+  $('.sidenav').sidenav();
+  $('select').formSelect();
+  $('.collapsible').collapsible();
+  $('.tabs').tabs();
 });
