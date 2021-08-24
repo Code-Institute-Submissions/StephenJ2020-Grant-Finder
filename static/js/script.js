702352524b4f69d59d1c031d70860456768853d3
Stephen <sjohnston.ie@gmail.com>
2021-08-24 00:00:30 +0000

    feat: Add jquery hide and show to tabs on grants.html git push

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,30 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav();
-    $('select').formSelect();
-    $('.collapsible').collapsible();
-    $('.tabs').tabs();
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+  $('select').formSelect();
+  $('.collapsible').collapsible();
+  $('.tabs').tabs();
+
+  $('#tab1').show();
+  $('#tab2').hide();
+  $('#tab3').hide();
+
+  $('#tab1').click(function () {
+    $('#tab1').show();
+    $('tab2').hide();
+    $('tab3').hide();
   });
+
+  $('#tab2').click(function () {
+    $('#tab1').hide();
+    $('tab2').show();
+    $('tab3').hide();
+  });
+
+  $('#tab3').click(function () {
+    $('#tab1').hide();
+    $('tab2').hide();
+    $('tab3').show();
+  });
+
+
+});
