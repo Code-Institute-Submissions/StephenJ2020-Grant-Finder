8d3399a36ec09d5cf318dcd2bedf627a14b4b0cb
Stephen <sjohnston.ie@gmail.com>
2021-08-25 19:02:45 +0000

    feat: Correct layout of General Info Tab on grants.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,27 +1,4 @@
 $(document).ready(function () {
-  
-  $('#tab1').show();
-  $('#tab2').hide();
-  $('#tab3').hide();
-
-  $('tab1').click(function () {
-    $('#tab1').show();
-    $('#tab2').show();
-    $('#tab3').show();
-  });
-
-  $('tab2').click(function () {
-    $('#tab1').hide();
-    $('#tab2').show();
-    $('#tab3').hide();
-  });
-
-  $('tab3').click(function () {
-    $('#tab1').hide();
-    $('#tab2').hide();
-    $('#tab3').show();
-  });
-
   $('.sidenav').sidenav();
   $('select').formSelect();
   $('.collapsible').collapsible();
