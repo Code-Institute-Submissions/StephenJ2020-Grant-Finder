e481fcbd27a3651870ed23e81ed6fbb26ff6bc9c
Stephen <sjohnston.ie@gmail.com>
2021-09-02 22:01:12 +0000

    maint: Remove unnecessary code from app.py

--- app.py
+++ app.py
@@ -289,12 +289,3 @@ if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
-
-
-for grant in grants:
-
-    counter = 0
-
-    while (counter <= grants):
-        print(counter)
-        counter += 1
